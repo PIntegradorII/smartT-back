a7a3355dd89886ea903a9a9c5e28222e5895a47a
valentinabarbetty <valentinabarbetty2@gmail.com>
2025-03-17 13:41:50 -0500

    correcciones cors

--- app/models/health.py
+++ app/models/health.py
@@ -9,7 +9,7 @@ class HealthData(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     tiene_condiciones = Column(String, nullable=False)
-    detalles_condiciones = Column(Text, nullable=True)
+    detalles_condiciones = Column(String, nullable=True)
     lesiones = Column(String, nullable=True)
     confirmacion = Column(String, nullable=False)
 
