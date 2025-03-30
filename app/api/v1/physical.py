0fbd081f6f11692ab1f904b2d46ea8c100b8d06c
valentinabarbetty <valentinabarbetty2@gmail.com>
2025-03-29 20:48:28 -0500

    cambios en el back para que haga update de datos fisicos con el id del usuario

--- app/api/v1/physical.py
+++ app/api/v1/physical.py
@@ -16,12 +16,13 @@ def create_physical_data(data: PhysicalDataCreate, db: Session = Depends(get_db)
     return new_data
 
 # Obtener un registro por ID
-@router.get("/physical_data/{id}", response_model=PhysicalDataResponse)
-def get_physical_data(id: int, db: Session = Depends(get_db)):
-    physical_data = db.query(PhysicalData).filter(PhysicalData.id == id).first()
+@router.get("/physical_data/{user_id}", response_model=PhysicalDataResponse)
+def get_physical_data(user_id: int, db: Session = Depends(get_db)):
+    physical_data = db.query(PhysicalData).filter(PhysicalData.user_id == user_id).first()
     if not physical_data:
         raise HTTPException(status_code=404, detail="Physical data not found")
     return physical_data
+
 
 # Obtener todos los registros
 @router.get("/", response_model=list[PhysicalDataResponse])
@@ -29,15 +30,18 @@ def get_all_physical_data(db: Session = Depends(get_db)):
     return db.query(PhysicalData).all()
 
 # Actualizar un registro por ID
-@router.put("/update/{id}", response_model=PhysicalDataResponse)
-def update_physical_data(id: int, data: PhysicalDataCreate, db: Session = Depends(get_db)):
-    physical_data = db.query(PhysicalData).filter(PhysicalData.id == id).first()
+@router.put("/update/{user_id}", response_model=PhysicalDataResponse)
+def update_physical_data(user_id: int, data: PhysicalDataCreate, db: Session = Depends(get_db)):
+    physical_data = db.query(PhysicalData).filter(PhysicalData.user_id == user_id).first()
     if not physical_data:
         raise HTTPException(status_code=404, detail="Physical data not found")
+    
     for key, value in data.dict().items():
         setattr(physical_data, key, value)
+    
     db.commit()
     db.refresh(physical_data)
+    
     return physical_data
 
 # Eliminar un registro por ID
