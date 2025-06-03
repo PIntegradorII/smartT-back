35211b1f6fd4c72b82a05e8df1a8bb0ad22a6d7b
Hector Diaz <hectorluis_d@hotmail.es>
2025-06-02 20:42:59 -0500

    Ajuste requirements

--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Actualizar paquetes y agregar soporte para locales
-apt-get update && apt-get install -y locales && locale-gen es_ES.UTF-8
+# Actualizar paquetes y agregar soporte para locales y libzbar
+apt-get update && apt-get install -y locales libzbar0
+
+# Generar el locale en español
+locale-gen es_ES.UTF-8
 
 # Exportar el locale para que Python lo use
 export LANG=es_ES.UTF-8
 export LC_ALL=es_ES.UTF-8
+
+# Instalar dependencias de Python
+pip install -r requirements.txt
